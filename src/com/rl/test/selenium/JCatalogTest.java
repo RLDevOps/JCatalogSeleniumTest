4cf2e2c5321e7edcc9012df240cc3da109be8374
RLDevOps <sandeep.ranganath@relevancelab.com>
2013-12-12 13:59:22 +0530

    Update JCatalogTest.java

--- src/com/rl/test/selenium/JCatalogTest.java
+++ src/com/rl/test/selenium/JCatalogTest.java
@@ -15,8 +15,8 @@ public class JCatalogTest extends MainDriver{
 		//System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
 		//driver = new InternetExplorerDriver();
 		//System.setProperty("webdriver.firefox.bin","/usr/bin/firefox");
-		driver = new FirefoxDriver();
-		// driver = new HtmlUnitDriver();
+		//driver = new FirefoxDriver();
+		 driver = new HtmlUnitDriver();
 		// driver.setJavascriptEnabled(true);
 		driver.get("http://192.168.103.30:8080/catalog/");		
 	}
